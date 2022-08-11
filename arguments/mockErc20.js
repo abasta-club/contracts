6b90391dae7b99b3ff226746a267eb82854dc068
Alvaro Fariña <farinalvaro@gmail.com>
2022-08-11 11:01:26 -0300

    fix: avoids reading env on every config access

--- arguments/mockErc20.js
+++ arguments/mockErc20.js
@@ -1,11 +1,8 @@
 const networkConfig = require("../networkConfig")
 
-// FIXME: shouldn't be needed to get the network name, the config should export network data only
-const env = process.env.NETWORK_NAME || "local"
-
 const {
   erc20PaymentToken: { symbol, name, initialSupply },
-} = networkConfig[env]
+} = networkConfig
 
 console.log("Verifying Mock ERC20 Payment token contract...")
 console.log({ symbol, name, initialSupply })
--- scripts/mockErc20/deploy.ts
+++ scripts/mockErc20/deploy.ts
@@ -2,18 +2,16 @@ import { ethers, network } from "hardhat"
 
 import networkConfig from "../../networkConfig"
 
-// FIXME: shouldn't be needed to get the network name, the config should export network data only
-const env: string = process.env.NETWORK_NAME || "local"
-
 const {
   erc20PaymentToken: { symbol, name, initialSupply },
-} = networkConfig[env]
+} = networkConfig
 
 async function main() {
   console.log("🚀 About to deploy MockERC20 token")
   console.log("===========================================================")
-  console.log(`name: ${name}`)
+  console.log(`network: ${network.name}`)
   console.log(`symbol: ${symbol}`)
+  console.log(`name: ${name}`)
   console.log(`initialSupply: ${initialSupply}`)
   const ERC20PaymentToken = await ethers.getContractFactory("MockERC20")
   const erc20PaymentToken = await ERC20PaymentToken.deploy(symbol, name, initialSupply)
